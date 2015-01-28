226bf783246b89dfa4f50250cb5f81fe22c6f078
Margo Lanier <mplanier@live.unc.edu>
2015-01-28 01:43:56 -0500

    added the *condensed* plot of scandal

---
+++ assignment4.py
@@ -0,0 +1,20 @@
+
+# Margo's version of Scandal
+
+protagonist = 'Olivia Pope'
+career = 'fixer'
+adjective1 = 'rich'
+adjective2 = 'powerful'
+number_of_seasons = 4
+episodes_per_season = 15
+
+print '%s is a %s.' % (protagonist, career)
+print 'She is very', adjective1, 'and', adjective2, 'because she fixes problems for', adjective1, 'and', adjective2, 'people.'
+print 'There have been %d seasons of Scandal and about %d episodes per season, so there have been about %d total episodes.' % (number_of_seasons, episodes_per_season, number_of_seasons * episodes_per_season)
+print 'If Olivia has a new client every other episode, she has had about %d clients.' % (number_of_seasons * episodes_per_season / 2)
+print 'In the evenings, Olivia often drinks 1 large glass of wine, then 3 more... and maybe 1 more glass, until she has consumed %d glasses (basically 1 bottle?)' % (1 + 3 + 1)
+print 'But then she eats a bag of popcorn, which cancels out the alcohol percentage of one of the glasses, so its really like she drank %d and consequently she is not drunk when she sneaks off to have her unabashed affair with the POTUS.' % (5 - 1)
+print 'Then Fitz made a deal with Olivia that if she correctly calculated 5 to the 18th power, he would kick his bosslady wife out of the White House for good.'
+print 'Olivia, being the intelligent woman she is, obviously would have answered with %d, but Cyrus killed her first, and Mellie took over the presidency.  The end.' % (5 ** 18)
+
+#SCANDALous
